02be7128fc07fd2e62a266eb288a716a2ae75015
TatianaKrivonos <konan_air.96@mail.ru>
2018-12-01 23:13:24 +0300

    Изменение файловой структуры

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ function createCards (dataProducts) {
 							<h3 class="product-card-mini__title">${product.title}</h3>
 						</a>
 						<a href="${product.link}" class="product-card-mini__img-wrap">
-							<img src="../epic-book-store/BOOK_STORE/books/${product.image}" alt="${product.title}" class="product-card-mini__img">
+							<img src="../epic-book-store/img/books/${product.image}" alt="${product.title}" class="product-card-mini__img">
 						</a>
 						<p class="product-card-mini__descr">${product.description}</p>
 						<div class="product-card-mini__price">${product.price} P</div>
