28ca39b61d929ca513e281cbd15b69df5752d201
TatianaKrivonos <konan_air.96@mail.ru>
2018-12-10 20:54:38 +0300

    Созданы новые модули для js, изменены файлы scss и css

---
+++ js/modules/sendRequest.js
@@ -0,0 +1,20 @@
+function sendRequest(data, onSuccess){
+		let xhr = new XMLHttpRequest;
+
+		xhr.open('GET', data);
+
+		xhr.send();
+
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState === 4 && xhr.status === 200) {
+				
+				const responseObj = JSON.parse(xhr.responseText);
+				onSuccess(responseObj);
+
+			} else {
+				console.log(`Жду загрузки: ${xhr.readyState}`);
+			}
+		}
+	};
+
+	export default sendRequest;
--- js/script.js
+++ js/script.js
@@ -14,14 +14,34 @@
 import insertElement from './modules/insertElement.js';
 import dataProducts from './modules/dataProducts.js';
 import bookCardTemplate from './modules/bookCardTemplate.js';
+import sendRequest from './modules/sendRequest.js';
+import createDataAjax from './modules/createDataAjax.js';
 
-
+	//объект данныхдля ajax запроса
 	const data = {
 		page: 1,
 		perPage: 8,
 		type: ''
 	};
 
+	const wrap = document.querySelector(bookCardTemplate.wrap);
+	if(wrap) {
+		const dataAjax = createDataAjax();
+
+		sendRequest(dataAjax, function(responseObj){
+			if (wrap.children) {
+				wrap.innerHTML = '';
+			}
+
+		insertElement(responseObj.items, bookCardTemplate);	
+	});
+	}	
+
+	// console.log(sendRequest(createDataAjax()));
+
+		
+
+	//вешаем слушатели на табы
 	const tabsWrap = document.querySelector('.j-tabs');
 	const tabsArray = Array.from(tabsWrap.children);
 
@@ -31,14 +51,7 @@ import bookCardTemplate from './modules/bookCardTemplate.js';
 			event.preventDefault();
 			data.type = event.target.dataset.type;
 
-			if (window.matchMedia("(min-width: 768px)").matches) {
-				data.perPage = 8;
-			} else {
-				data.perPage = 3;
-			}
-
-			const dataAjax = `https://api.do-epixx.ru/htmlpro/bookstore/books/get/${data.page}/${data.perPage}/${data.type}`;
-			console.log(dataAjax);
+			const dataAjax = createDataAjax();
 
 			sendRequest(dataAjax);
 
@@ -46,34 +59,44 @@ import bookCardTemplate from './modules/bookCardTemplate.js';
 		
 	});
 
-	function sendRequest(data){
-		let xhr = new XMLHttpRequest;
-
-		xhr.open('GET', data);
-
-		xhr.send();
-
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState === 4 && xhr.status === 200) {
-				const request = JSON.parse(xhr.responseText);
-				console.log(request);
-
-				if (document.querySelector(bookCardTemplate.wrap)) {
-					const wrap = document.querySelector(bookCardTemplate.wrap);
-
-					if (wrap.children) {
-						wrap.innerHTML = '';
-					}
-					
-					insertElement(request.items, bookCardTemplate);
-				}
-
-			} else {
-				// console.log(xhr.responseText);
-				console.log(`Жду загрузки: ${xhr.readyState}`);
-			}
+	//функция подготовки url'a для GET запроса
+	function createDataAjax(){
+		if (window.matchMedia("(min-width: 768px)").matches) {
+			data.perPage = 8;
+		} else {
+			data.perPage = 3;
 		}
-	};
+		return `https://api.do-epixx.ru/htmlpro/bookstore/books/get/${data.page}/${data.perPage}/${data.type}`;
+	}
+
+	// function sendRequest(data){
+	// 		let xhr = new XMLHttpRequest;
+
+	// 		xhr.open('GET', data);
+
+	// 		xhr.send();
+
+	// 		xhr.onreadystatechange = function(){
+	// 			if(xhr.readyState === 4 && xhr.status === 200) {
+	// 				return xhr.responseText;
+	// 				const request = JSON.parse(xhr.responseText);
+
+	// 				if (document.querySelector(bookCardTemplate.wrap)) {
+	// 					const wrap = document.querySelector(bookCardTemplate.wrap);
+
+	// 					if (wrap.children) {
+	// 						wrap.innerHTML = '';
+	// 					}
+
+	// 					insertElement(request.items, bookCardTemplate);
+	// 				}
+
+	// 			} else {
+	// 				console.log(`Жду загрузки: ${xhr.readyState}`);
+	// 			}
+	// 		}
+	// 	};
+	
 
 
 	
