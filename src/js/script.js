fe21eb2fc79399802454b472e8fd42eb50237d72
TatianaKrivonos <konan_air.96@mail.ru>
2018-12-13 22:20:03 +0300

    исправлен js для табов

--- src/js/script.js
+++ src/js/script.js
@@ -13,63 +13,94 @@
 
 import insertElement from '../js/modules/insertElement.js';
 import bookCardTemplate from '../js/modules/bookCardTemplate.js';
-import sendRequest from '../js/modules/sendRequest.js';
-// import createDataAjax from '../js/modules/createDataAjax.js';
-// import createEventListener from '../js/modules/createEventListener.js'
 
+//объект данныхдля ajax запроса
+const data = {
+	page: 1,
+	perPage: 8,
+	type: ''
+};
 
+const wrap = document.querySelector(bookCardTemplate.wrap);
+if(wrap) {
+	const dataAjax = createDataAjax();
 
-	//объект данныхдля ajax запроса
-	const data = {
-		page: 1,
-		perPage: 8,
-		type: ''
-	};
+	sendRequest(dataAjax, function(responseObj){
+		if (wrap.children) {
+			wrap.innerHTML = '';
+		}
 
-	const wrap = document.querySelector(bookCardTemplate.wrap);
-	if(wrap) {
-		const dataAjax = createDataAjax();
-
-		sendRequest(dataAjax, function(responseObj){
-			if (wrap.children) {
-				wrap.innerHTML = '';
-			}
-
-		insertElement(responseObj.items, bookCardTemplate);	
-		});
-	}	
-
-	// console.log(sendRequest(createDataAjax()));
-
-		
-
-	//вешаем слушатели на табы
-	const tabsWrap = document.querySelector('.j-tabs');
-	const tabsArray = Array.from(tabsWrap.children);
-
-	tabsArray.forEach(function(tab) {
-		const link = tab.firstElementChild;
-		link.addEventListener('click', function(event) {
-			event.preventDefault();
-			data.type = event.target.dataset.type;
-
-			const dataAjax = createDataAjax();
-
-			sendRequest(dataAjax);
-
-		});
-		
+	insertElement(responseObj.items, bookCardTemplate);	
 	});
+}	
 
-	//функция подготовки url'a для GET запроса
-	function createDataAjax(){
+console.log(sendRequest(createDataAjax()));
+
+	
+
+//функция подготовки url'a для GET запроса
+function createDataAjax(){
+	if (window.matchMedia("(min-width: 768px)").matches) {
+		data.perPage = 8;
+	} else {
+		data.perPage = 3;
+	}
+	return `https://api.do-epixx.ru/htmlpro/bookstore/books/get/${data.page}/${data.perPage}/${data.type}`;
+}
+
+//вешаем слушатели на табы
+const tabsWrap = document.querySelector('.j-tabs');
+const tabsArray = Array.from(tabsWrap.children);
+
+tabsArray.forEach(function(tab) {
+	const link = tab.firstElementChild;
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		data.type = event.target.dataset.type;
+
 		if (window.matchMedia("(min-width: 768px)").matches) {
 			data.perPage = 8;
 		} else {
 			data.perPage = 3;
 		}
-		return `https://api.do-epixx.ru/htmlpro/bookstore/books/get/${data.page}/${data.perPage}/${data.type}`;
+
+		const dataAjax = `https://api.do-epixx.ru/htmlpro/bookstore/books/get/${data.page}/${data.perPage}/${data.type}`;
+		console.log(dataAjax);
+
+		sendRequest(dataAjax);
+
+	});
+	
+});
+
+function sendRequest(data){
+	let xhr = new XMLHttpRequest;
+
+	xhr.open('GET', data);
+
+	xhr.send();
+
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4 && xhr.status === 200) {
+			const request = JSON.parse(xhr.responseText);
+			console.log(request);
+
+			if (document.querySelector(bookCardTemplate.wrap)) {
+				const wrap = document.querySelector(bookCardTemplate.wrap);
+
+				if (wrap.children) {
+					wrap.innerHTML = '';
+				}
+				
+				insertElement(request.items, bookCardTemplate);
+			}
+
+		} else {
+			// console.log(xhr.responseText);
+			console.log(`Жду загрузки: ${xhr.readyState}`);
+		}
 	}
+};
 
 		
 
