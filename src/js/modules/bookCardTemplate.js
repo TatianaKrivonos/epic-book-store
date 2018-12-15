d0951bb598f0bac4612b1d1bfe72c7385be4af0f
TatianaKrivonos <konan_air.96@mail.ru>
2018-12-15 15:11:14 +0300

    изменено название блока с карточками товара

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -2,16 +2,16 @@
 const bookCardTemplate = {
 	wrap: '.catalog__list',
 	tag: 'article',
-	tagClass: 'product-card-mini',
+	tagClass: 'product-card',
 	setContent: function(product) {
 		return `<a href="${product.link}">
-			       <h3 class="product-card-mini__title">${product.name}</h3>
+			       <h3 class="product-card__title">${product.name}</h3>
 			    </a>
-				<a href="${product.link}" class="product-card-mini__img-wrap">
+				<a href="${product.link}" class="product-card__img-wrap">
 					<img src="./img/books_all/${product.uri}.png" alt="${product.name}" class="product-card-mini__img">
 				</a>
-				<p class="product-card-mini__descr">${product.desc}</p>
-					<div class="product-card-mini__price">${product.price} P</div>`;
+				<p class="product-card__descr">${product.desc}</p>
+					<div class="product-card__price">${product.price} P</div>`;
 	}
 }
 
