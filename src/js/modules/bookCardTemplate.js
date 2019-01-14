b1437b6e0f7a610e85f7b06642a96d3fe2cc56eb
TatianaKrivonos <konan_air.96@mail.ru>
2019-01-14 12:29:11 +0300

    исправлена сетка в каталоге и в блоке с доставкой

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -1,17 +1,19 @@
 
 const bookCardTemplate = {
 	wrap: '.catalog__list',
-	tag: 'article',
-	tagClass: 'product-card',
+	tag: 'div',
+	tagClass: 'catalog__col',
 	setContent: function(product) {
-		return `<a href="${product.link}">
-			       <h3 class="product-card__title">${product.name}</h3>
-			    </a>
-				<a href="books.html" class="product-card__img-wrap">
-					<img src="./img/books_all/${product.uri}.png" alt="${product.name}" class="product-card__img">
-				</a>
-				<p class="product-card__descr">${product.desc}</p>
-					<div class="product-card__price">${product.price} &#8381;</div>`;
+		return `<article class="product-card">
+              <a href="books.html">
+			         <h3 class="product-card__title">${product.name}</h3>
+			        </a>
+				      <a href="books.html" class="product-card__img-wrap">
+					      <img src="./img/books_all/${product.uri}.png" alt="${product.name}" class="product-card__img">
+				      </a>
+				      <p class="product-card__descr">${product.desc}</p>
+					    <div class="product-card__price">${product.price} &#8381;</div>
+            </article>`;
 	}
 }
 
